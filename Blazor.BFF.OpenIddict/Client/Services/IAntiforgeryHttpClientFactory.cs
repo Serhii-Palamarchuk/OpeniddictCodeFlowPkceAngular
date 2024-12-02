ca184e229cecf2eab1923a5a4ccee11b5a80b03e
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2024-12-02 03:53:42 +0200

    clean

--- Blazor.BFF.OpenIddict/Client/Services/IAntiforgeryHttpClientFactory.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Blazor.BFF.OpenIddict.Client.Services;
-
-public interface IAntiforgeryHttpClientFactory
-{
-    Task<HttpClient> CreateClientAsync(string clientName = "authorizedClient");
-}
